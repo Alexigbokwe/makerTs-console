92997cee56c19f8f9d79aff26582afa2d4162c8d
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-23 13:16:30 +0100

    fix: provider command description

--- src/commands/serviceProvider/index.ts
+++ src/commands/serviceProvider/index.ts
@@ -5,7 +5,7 @@ class providerCommand {
   static async handle(program:any) {
     await program
       .command("make-provider [providerName]")
-      .description("Create a new job class")
+      .description("Create new service provider class")
       .action((providerName:string) => {
         providerProgram.handle(providerName);
       });
