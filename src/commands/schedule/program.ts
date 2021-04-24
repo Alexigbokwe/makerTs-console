2e23dfea2ada24ae812b3fe579ff2ba4b8588637
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-24 15:44:06 +0100

    fix: scheduler command

--- src/commands/schedule/program.ts
+++ src/commands/schedule/program.ts
@@ -1,16 +1,25 @@
 "use strict";
 const pathTo = process.env.PWD;
 import BaseCommand from "../baseCommand";
+import shell from "shelljs";
 
 class ScheduledProgram {
   static async handle() {
-    let path = `${pathTo}/App/Console/kernel.ts`;
+    this.buildFile()
+    let path = `${pathTo}/buildApp/Console/kernel.js`;
     let kernel = require(path);
     try {
       BaseCommand.success("Running scheduled command");
       kernel.schedule();
     } catch (error) {
       BaseCommand.error(error);
+    }
+  }
+
+  private static buildFile() {
+    if (shell.exec("tsc -p .").code !== 0) {
+      shell.echo("Error: Build project command failed");
+      shell.exit(1);
     }
   }
 }
