0d8f98f16a8d44a9217ced9d9303c7ae79cedf2a
Alex Igbokwe <juvitafrica@gmail.com>
2021-05-30 06:33:41 +0100

    identity build

---
+++ lib/src/commands/identity/program.d.ts
@@ -0,0 +1,12 @@
+declare class IdentityProgram {
+    static handle(): Promise<void>;
+    private static ProcessSqlIdentity;
+    private static ProcessNoSqlIdentity;
+    private static checkDatabaseDriver;
+    private static generateModel;
+    private static nextStep;
+    private static generateNoSqlModel;
+    private static generateSqlModel;
+}
+export default IdentityProgram;
+//# sourceMappingURL=program.d.ts.map
