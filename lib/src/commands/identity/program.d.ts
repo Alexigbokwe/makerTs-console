848584aa0d88ea649bbe7fdeb81c31ac318c4121
Alex Igbokwe <juvitafrica@gmail.com>
2021-06-01 11:32:44 +0100

    new build

--- lib/src/commands/identity/program.d.ts
+++ lib/src/commands/identity/program.d.ts
@@ -1,5 +1,11 @@
 declare class IdentityProgram {
     static handle(): Promise<void>;
+    private static ProcessSqlIdentitySchema;
+    private static createSchemaFile;
+    private static PermissionSchema;
+    private static RoleSchema;
+    private static Role_PermissionSchema;
+    private static User_RoleSchema;
     private static ProcessSqlIdentity;
     private static ProcessNoSqlIdentity;
     private static checkDatabaseDriver;
