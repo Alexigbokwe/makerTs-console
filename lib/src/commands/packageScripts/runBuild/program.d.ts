ef76fb4013002967f6fec8dc8bc408dcdb66dc5c
Alex Igbokwe <juvitafrica@gmail.com>
2021-07-27 03:30:02 +0100

    feature: copy static folders in build command

--- lib/src/commands/packageScripts/runBuild/program.d.ts
+++ lib/src/commands/packageScripts/runBuild/program.d.ts
@@ -1,6 +1,7 @@
 declare class CompileProgramProgram {
     static handle(): void;
     private static buildFile;
+    private static copyStaticDirectories;
 }
 export default CompileProgramProgram;
 //# sourceMappingURL=program.d.ts.map
--- src/commands/packageScripts/runBuild/program.ts
+++ src/commands/packageScripts/runBuild/program.ts
@@ -1,4 +1,8 @@
 import shell from "shelljs";
+import copy from "recursive-copy";
+
+//@ts-ignore
+import config from "./Config/app.ts";
 class CompileProgramProgram {
   static handle() {
     this.buildFile();
@@ -8,7 +12,24 @@ class CompileProgramProgram {
     if (shell.exec("rimraf ./build && tsc -p .").code !== 0) {
       shell.echo("Error: Build project command failed");
       shell.exit(1);
+    } else {
+      let directories = config.static_directories;
+      if (directories.lenght > 0) {
+        this.copyStaticDirectories(directories);
+      }
     }
+  }
+
+  private static copyStaticDirectories(directories: Array<{ name: string; source: string; destination: string }>) {
+    directories.map((directory: { name: string; source: string; destination: string }) => {
+      copy("./" + directory.source, "./" + directory.destination)
+        .then(() => {
+          console.info("Copied " + directory.name + " to" + directory.destination);
+        })
+        .catch(function (error) {
+          console.error("Copy failed: " + error);
+        });
+    });
   }
 }
 
