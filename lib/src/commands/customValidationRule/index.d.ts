457d1acbd19101fb45e1cc7abe23c502a57fffed
Alex Igbokwe <juvitafrica@gmail.com>
2021-12-25 18:37:16 +0100

    new build

---
+++ lib/src/commands/customValidationRule/index.d.ts
@@ -0,0 +1,5 @@
+declare class RuleCommand {
+    static handle(program: any): Promise<void>;
+}
+export default RuleCommand;
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/src/commands/customValidationRule/program.d.ts
@@ -0,0 +1,7 @@
+declare class CustomValidationRuleProgram {
+    static handle(name: string): Promise<void>;
+    private static nextStep;
+    private static generateRule;
+}
+export default CustomValidationRuleProgram;
+//# sourceMappingURL=program.d.ts.map
